22c1666bf5d0499d728c0e8cb3c534f58f9e7f0f
Shamim Ehsan <ehsanhrid@gmail.com>
2018-06-03 01:20:34 +0600

    add a route

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(bodyparser.raw({extended: true}));
 
 var db;
 const mongoClient = require('mongodb').MongoClient;
+var objectID = require('mongodb').ObjectID;
 const collectionName = 'mobileTest'
 mongoClient.connect('mongodb://ehsan:qwerty@ds119080.mlab.com:19080/ehsandb',(err, client)=>{
 	if(err) return console.log(err);
@@ -13,50 +14,49 @@ mongoClient.connect('mongodb://ehsan:qwerty@ds119080.mlab.com:19080/ehsandb',(er
 	app.listen(3000,'0.0.0.0');
 console.log("example app running");	
 })
-const objectID = require('mongodb').ObjectID;
 app.get('/', (req,res) =>{
-	db.collection(collectionName).find().toArray((err, result)=>{
-		res.send(result);
-	})
+    db.collection(collectionName).find().toArray((err, result)=>{
+        res.send(result);
+    })
 })
 app.get('/:id', (req, res) =>{
-	if(objectID.isValid(req.params.id)== false)
-		return res.status(400).send("Bad Request");
-	db.collection(collectionName).find({
-		_id: objectID(req.params.id)
-	}).toArray((err, result)=>{
-			if(result[0]!=null)
-				res.send(result);
-			else
-				res.status(400).send("Bad Request");	
+    if(objectID.isValid(req.params.id)== false)
+        return res.status(400).send("Bad Request");
+    db.collection(collectionName).find({
+        _id: objectID(req.params.id)
+    }).toArray((err, result)=>{
+            if(result[0]!=null)
+                res.send(result);
+            else
+                res.status(400).send("Bad Request");    
 
-	})
+    })
 })
 app.post('/', (req,res) =>{
 
-	db.collection(collectionName).find({
-		name: req.body.name,
-		price : req.body.price,
-		number : req.body.number
-	}).toArray((err,result) =>{
-		if (result[0]!=null)
-			return res.status(409).send("409 Duplicate ");
-		else{
-			db.collection(collectionName).save(req.body, (err, result)=>{
-				if(err) return console.log(err);
-				console.log("saved to database");
-				res.status(201).send(req.body); 
-			})
-		}
+    db.collection(collectionName).find({
+        name: req.body.name,
+        price : req.body.price,
+        number : req.body.number
+    }).toArray((err,result) =>{
+        if (result[0]!=null)
+            return res.status(409).send("409 Duplicate ");
+        else{
+            db.collection(collectionName).save(req.body, (err, result)=>{
+                if(err) return console.log(err);
+                console.log("saved to database");
+                res.status(201).send(req.body); 
+            })
+        }
 
-	})
+    })
 })
 app.put('/:id', function(req, res) {
     db.collection(collectionName)
         .findOneAndUpdate({
             "_id": objectID(req.params.id)
         }, {
-        	$set: {
+            $set: {
                 "name": req.body.name,
                 "price": req.body.price,
                 "mobile": req.body.mobile
@@ -67,14 +67,14 @@ app.put('/:id', function(req, res) {
         .findOneAndUpdate({
             "_id": objectID(req.params.id)
         }, {
-        	$unset: {
+            $unset: {
                 "name": 0,
                 "price": req.body.price,
                 "mobile": req.body.mobile
             }
     });
     
-	*/
+    */
         
     res.send(req.body);
 })
@@ -100,4 +100,6 @@ app.delete('/', function(req, res) {
     db.collection(collectionName).remove({});
     res.redirect('/');
 })
-
+app.all('*',function(req,res){
+    res.status(404).send("ERROR 400 Not Found!!!");
+})
