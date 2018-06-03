7c3bf3fbcf09814da2ddc9b35ed199c74b5e3570
Shamim Ehsan <ehsanhrid@gmail.com>
2018-06-03 21:23:01 +0600

    added error handling method

--- server.js
+++ server.js
@@ -113,3 +113,7 @@ app.all('*',(req,res) =>{
     res.status(404).send("ERROR 400!!!");
 })
 
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Internal Server error!')
+})
